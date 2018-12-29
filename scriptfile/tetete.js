5fbd3842334ddd0c85670ea544bad94c338ac983
idoushiki <idoushiki111@gmail.com>
2018-12-29 13:18:57 +0900

    a

--- scriptfile/tetete.js
+++ scriptfile/tetete.js
@@ -10,12 +10,7 @@ var num=[1];
 //window.onload=function(){
   var canvas = document.getElementById('c');
     var ctx = canvas.getContext('2d');
-/*
-    document.body.onclick = function() {
-      ctx.drawImage(img, mX, mY)
-    };
- });
-}*/
+
 canvas.addEventListener('click', onClick, false);
 // number bf picture
 function onClick(e) {
@@ -33,4 +28,40 @@ function HozonImg()
 {
  var png = canvas.toDataURL();
   document.getElementById("newImg").src = png;
+}
+
+function clc(){//clear picture
+//キャンバスクリアする
+ctx.clearRect(0, 0, 800, 800);
+}
+
+
+lineWidth.addEventListener("mousemove",function(){//size of picture
+
+  document.getElementById("image").style.cursor = 'url("img/1.png"),auto';
+
+var lineNum = document.getElementById("lineWidth").value;
+document.getElementById("lineNum").innerHTML = lineNum;
+
+var targetImg = document.getElementById('image');
+
+var orgWidth  = targetImg.width;
+var orgHeight  = targetImg.orgHeight;
+
+});
+
+
+function onClick(e) {
+
+
+  size=  document.getElementById("lineNum").innerHTML;
+  var img=new Image();
+  img.src="img/"+num+".png";
+  var rect = e.target.getBoundingClientRect();
+      x = e.clientX*2- rect.left-(size/2);
+      y = e.clientY*2- rect.top-(size);
+
+
+  ctx.drawImage(img, x, y,size,size);
+
 }
